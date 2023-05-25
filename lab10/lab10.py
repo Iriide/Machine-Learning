d78e1abbb33d79570466fda0a460b93b72c398ad
asiawoj1 <asiawoj1@gmail.com>
2023-05-25 22:09:14 +0200

    lab10

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -84,6 +84,7 @@ X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full
 
 scaler = StandardScaler()
 
+
 X_train = scaler.fit_transform(X_train)
 X_valid = scaler.transform(X_valid)
 X_test = scaler.transform(X_test)
