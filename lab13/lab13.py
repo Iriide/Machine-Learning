4dc86b0a2737a8008e1e719bf46ed39d5f9db114
asiawoj1 <asiawoj1@gmail.com>
2023-06-22 20:30:56 +0200

    lab13

--- lab13/lab13.py
+++ lab13/lab13.py
@@ -17,35 +17,34 @@ from sklearn.metrics import mean_absolute_error
 
 """1  Pobieranie danych"""
 
-tf.keras.utils.get_file("bike_sharing_dataset.zip", "https://archive.ics.uci.edu/static/public/275/bike+sharing+dataset.zip", cache_dir=".", extract=True)
+tf.keras.utils.get_file("bike_sharing_dataset.zip",
+                        "https://archive.ics.uci.edu/static/public/275/bike+sharing+dataset.zip", cache_dir=".",
+                        extract=True)
 
-df=pd.read_csv('datasets/hour.csv',parse_dates={'datetime': ['dteday','hr']},date_format='%Y-%m-%d %H',index_col='datetime')
+df = pd.read_csv('datasets/hour.csv', parse_dates={'datetime': ['dteday', 'hr']}, date_format='%Y-%m-%d %H',
+                 index_col='datetime')
 print((df.index.min(), df.index.max()))
-print((365+366)*24-len(df))
+print((365 + 366) * 24 - len(df))
 
 df.notna().sum()
-df[['casual','registered','cnt','weathersit']].describe()
+df[['casual', 'registered', 'cnt', 'weathersit']].describe()
 
-df.casual/=1e3
-df.registered/=1e3
-df.cnt/=1e3
-df.weathersit/=4
+df.casual /= 1e3
+df.registered /= 1e3
+df.cnt /= 1e3
+df.weathersit /= 4
 
-df_2weeks= df[:24*7*2]
-df_2weeks[['casual','registered','cnt','temp']].plot(figsize=(10,3))
+df_2weeks = df[:24 * 7 * 2]
+df_2weeks[['casual', 'registered', 'cnt', 'temp']].plot(figsize=(10, 3))
 
-df_daily=df.resample('W').mean()
-df_daily[['casual','registered','cnt','temp']].plot(figsize=(10,3))
+df_daily = df.resample('W').mean()
+df_daily[['casual', 'registered', 'cnt', 'temp']].plot(figsize=(10, 3))
 
 """3  Wskaźniki bazowe"""
 
-df['cnt'] *= 1e3
-df['daily_lag'] = df['cnt'].shift(1)
-df['weekly_lag'] = df['cnt'].shift(7)
+mae_daily = df['cnt'].diff(24).abs().mean() * 1e3
+mae_weekly = df['cnt'].diff(24 * 7).abs().mean() * 1e3
 
-df = df.dropna()
-mae_daily = mean_absolute_error(df['cnt'], df['daily_lag'])
-mae_weekly = mean_absolute_error(df['cnt'], df['weekly_lag'])
 
 print((mae_daily, mae_weekly))
 with open('mae_baseline.pkl', 'wb') as f:
@@ -53,21 +52,23 @@ with open('mae_baseline.pkl', 'wb') as f:
 
 """4  Predykcja przy pomocy sieci gęstej"""
 
-cnt_train=df['cnt']['2011-01-01 00:00':'2012-06-30 23:00']
-cnt_valid=df['cnt']['2012-07-01 00:00':]
+cnt_train = df['cnt']['2011-01-01 00:00':'2012-06-30 23:00']
+cnt_valid = df['cnt']['2012-07-01 00:00':]
 
-seq_len=24
+seq_len = 24
 
-train_ds=tf.keras.utils.timeseries_dataset_from_array(cnt_train.to_numpy(),targets=cnt_train[seq_len:],sequence_length=seq_len,batch_size=32,shuffle=True,seed=42)
+train_ds = tf.keras.utils.timeseries_dataset_from_array(cnt_train.to_numpy(), targets=cnt_train[seq_len:],
+                                                        sequence_length=seq_len, batch_size=32, shuffle=True, seed=42)
 
-valid_ds=tf.keras.utils.timeseries_dataset_from_array(cnt_valid.to_numpy(),targets=cnt_valid[seq_len:],sequence_length=seq_len,batch_size=32)
+valid_ds = tf.keras.utils.timeseries_dataset_from_array(cnt_valid.to_numpy(), targets=cnt_valid[seq_len:],
+                                                        sequence_length=seq_len, batch_size=32)
 
-model=tf.keras.Sequential([tf.keras.layers.Dense(1, input_shape=[seq_len])])
+model = tf.keras.Sequential([tf.keras.layers.Dense(1, input_shape=[seq_len])])
 
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.05, momentum=0.9)
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.0001, momentum=0.9)
 loss = tf.keras.losses.Huber()
 
-model.compile(optimizer=optimizer, loss= loss, metrics='mae')
+model.compile(optimizer=optimizer, loss=loss, metrics='mae')
 history = model.fit(train_ds, validation_data=valid_ds, epochs=20)
 
 model.save('model_linear.h5')
@@ -78,16 +79,16 @@ mae_linear = mean_absolute_error(true_values, preds)
 
 print(mae_linear)
 with open('mae_linear.pkl', 'wb') as f:
-    pkl.dump((mae_linear,), f)
+    pkl.dump((mae_linear * 1e3), f)
 
 """5  Prosta sieć rekurencyjna"""
 
-model=tf.keras.Sequential([tf.keras.layers.SimpleRNN(1, input_shape=[None,1])])
+model = tf.keras.Sequential([tf.keras.layers.SimpleRNN(1, input_shape=[None, 1])])
 
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.2, momentum=0.9)
 loss = tf.keras.losses.Huber()
 
-model.compile(optimizer=optimizer, loss= loss, metrics='mae')
+model.compile(optimizer=optimizer, loss=loss, metrics='mae')
 history = model.fit(train_ds, validation_data=valid_ds, epochs=20)
 
 model.save("model_rnn1.h5")
@@ -100,9 +101,10 @@ print(mae_rnn)
 with open('mae_rnn1.pkl', 'wb') as f:
     pkl.dump((mae_rnn,), f)
 
-model_rnn32 = tf.keras.Sequential([tf.keras.layers.SimpleRNN(32, input_shape=[None, 1]),tf.keras.layers.Dense(1)])
+model_rnn32 = tf.keras.Sequential([tf.keras.layers.SimpleRNN(32, input_shape=[None, 1]), tf.keras.layers.Dense(1)])
 
-model_rnn32.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.2, momentum=0.9),loss=tf.keras.losses.Huber(),metrics=['mae'])
+model_rnn32.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0001, momentum=0.9), loss=tf.keras.losses.Huber(),
+                    metrics=['mae'])
 
 history = model_rnn32.fit(train_ds, validation_data=valid_ds, epochs=20)
 
@@ -123,7 +125,8 @@ model_rnn_deep = tf.keras.Sequential([
     tf.keras.layers.Dense(1)
 ])
 
-model_rnn_deep.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9),loss=tf.keras.losses.Huber(),metrics=['mae'])
+model_rnn_deep.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9),
+                       loss=tf.keras.losses.Huber(), metrics=['mae'])
 
 history = model_rnn_deep.fit(train_ds, validation_data=valid_ds, epochs=20)
 
@@ -178,4 +181,3 @@ mae_rnn_mv = mean_absolute_error(true_values_mv, preds_mv)
 
 with open('mae_rnn_mv.pkl', 'wb') as f:
     pkl.dump((mae_rnn_mv,), f)
-
